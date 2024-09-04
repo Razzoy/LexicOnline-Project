96ff72904e5c9d87707c919bd5bb018779009e8a
Razzoy <1081892@ucn.dk>
2024-09-05 00:50:01 +0200

    Last setup

--- Lexiconline/src/App.jsx
+++ Lexiconline/src/App.jsx
@@ -8,14 +8,15 @@ import { NoPage } from './pages/NoPage'
 
 function App() {
 
+  
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path={'/'} element={<MainLayout />}>
-          <Route index element={<Home/>}/>
-          <Route path={'/about'} element={<About/>}/>
-          <Route path={'/*'} element={<NoPage/>}/>  
+          <Route index element={<Home />} />
+          <Route path={'/about'} element={<About />} />
+          <Route path={'/*'} element={<NoPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- Lexiconline/src/pages/Home.jsx
+++ Lexiconline/src/pages/Home.jsx
@@ -1,7 +1,6 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export function Home() {
-
     useEffect(() => {
         console.log("Home page has been mounted");
         return () => {
@@ -10,9 +9,36 @@ export function Home() {
 
     }, [])
 
+    const [inputValue, setInputValue] = useState('');
+    const [wordData, setWordData] = useState(null);
+
+
+    const wordChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}`
+    async function fetchWord() {
+        if (!inputValue.trim()) return;
+
+        let res = await fetch(url)
+        let data = await res.json()
+        setWordData(data);
+        console.log(wordData);
+
+    }
+
+    useEffect(()=>{
+        
+    }, [inputValue])
+
+
     return (
         <>
-            
+            <input type="text" value={inputValue} onChange={wordChange} placeholder="Write a word"/>
+            <button onClick={() => fetchWord()}>Get Your Word!!</button>
+
         </>
     )
 }
