a5d16e7e2f554bbf2d4330a00ed621ac12a34b28
Razzoy <1081892@ucn.dk>
2024-09-09 09:10:25 +0200

    almost done

--- Lexiconline/src/components/PaperWrapper/HomePaper/HomePaper.jsx
+++ Lexiconline/src/components/PaperWrapper/HomePaper/HomePaper.jsx
@@ -13,27 +13,29 @@ export function HomePaper({ wordData }) {
                 </span>
                 {wordData ? <h2>{wordData.word}</h2> : <h2>word</h2>}
             </header>
-            <section className={style.Noun}>
+            <section className={style.noun}>
                 <h3>Noun:</h3>
                 {wordData ? wordData?.meanings?.map((key)=> {
                     {return key.synonyms.map((syn)=> {
                         return <p key={syn}>"{syn}"</p>
                     })}
-                }): <p>nouns</p>}
+                }): <p>"nouns"</p>}
             </section>
 
-            <section className={style.Verb}>
+            <section className={style.verb}>
                 <h3>Verb:</h3>
                 {wordData ? wordData?.meanings[1]?.definitions?.map((def)=> {
-                        return <p key={def}>{def.definition}</p>
+                        return <p key={def}>"{def.definition}"</p>
                     })
-                : <p>verbs</p>}
+                : <p>"verbs"</p>}
             </section>
 
-            <section className={style.Interjection}>
-                <h3>Interjection:</h3>
-                {wordData ? <p>{wordData.meanings[0].definitions[0].example}</p> : null} 
-
+            <section className={style.interjection}>
+                <h3>Adjective:</h3>
+                {wordData ? wordData?.meanings[2]?.definitions?.map((adj)=> {
+                        return <p key={adj}>"{adj.definition}"</p>
+                    })
+                : <p>"adjectives"</p>}
             </section>
         </div>
     )
